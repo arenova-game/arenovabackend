3d0790b51be913acd649482f4b36aaf2e5b31c80
konissi ricky <konissiricky@gmail.com>
2026-09-09 07:35:17 +0100

    chore: serve swagger ui at root path

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('', app, document, {
     customSiteTitle: 'Arenova API Documentation',
     swaggerOptions: {
       persistAuthorization: true,
